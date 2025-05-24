f7dc6b7ab6d32a314f20a361b616d798948d2353
ATurekhanov <turekhanovabzal3@gmail.com>
2025-05-24 09:19:28 +0500

    9.1 Рефакторинг и подготовка к написанию API автотестов. Практика реализации фикстур

---
+++ fixtures/exercises.py
@@ -0,0 +1,24 @@
+import pytest
+from pydantic import BaseModel
+
+from clients.exercises.exercises_client import get_exercises_client, ExercisesClient
+from clients.exercises.exercises_schema import CreateExerciseRequestSchema, ExerciseResponseSchema
+from fixtures.files import FileFixture
+from fixtures.users import UserFixture
+
+
+class ExerciseFixture(BaseModel):
+    request: CreateExerciseRequestSchema
+    response: ExerciseResponseSchema
+
+
+@pytest.fixture
+def exercises_client(func_user: UserFixture) -> ExercisesClient:
+    return get_exercises_client(func_user.authentication_user)
+
+
+@pytest.fixture
+def func_exercise(exercises_client: ExercisesClient, func_course: FileFixture) -> ExerciseFixture:
+    request = CreateExerciseRequestSchema(courseId=func_course.id)
+    response = exercises_client.create_exercise(request)
+    return ExerciseFixture(request=request, response=response)
